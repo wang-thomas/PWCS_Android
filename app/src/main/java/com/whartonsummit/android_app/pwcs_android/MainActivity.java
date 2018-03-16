0a840ca813544aca0afeb3970c704d819faf61dd
zhileiz <zhileiz@seas.upenn.edu>
2018-03-16 17:57:50 -0400

    initial commit

---
+++ app/src/main/java/com/whartonsummit/android_app/pwcs_android/MainActivity.java
@@ -0,0 +1,37 @@
+package com.whartonsummit.android_app.pwcs_android;
+
+import android.net.Uri;
+import android.support.v4.view.ViewPager;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.Toolbar;
+
+import com.ogaclejapan.smarttablayout.SmartTabLayout;
+import com.ogaclejapan.smarttablayout.utils.v4.FragmentPagerItemAdapter;
+import com.ogaclejapan.smarttablayout.utils.v4.FragmentPagerItems;
+
+public class MainActivity extends AppCompatActivity implements TestBlankFragment.OnFragmentInteractionListener {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        FragmentPagerItemAdapter myAdapter = new FragmentPagerItemAdapter(
+                getSupportFragmentManager(), FragmentPagerItems.with(this)
+                .add("Hello", TestBlankFragment.class)
+                .add("Abraham", TestBlankFragment.class)
+                .add("Babson", TestBlankFragment.class)
+                .create());
+
+        ViewPager viewPager = (ViewPager) findViewById(R.id.viewpager);
+        viewPager.setAdapter(myAdapter);
+
+        SmartTabLayout viewPagerTab = (SmartTabLayout) findViewById(R.id.viewpagertab);
+        viewPagerTab.setViewPager(viewPager);
+    }
+
+    @Override
+    public void onTestFragmentInteraction(Uri string) {
+
+    }
+}
